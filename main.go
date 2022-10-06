b0f15100028d2f1d78650208f6ff707ac60df0e2
Roman Kislov <roki@boozt.com>
2022-10-06 23:17:13 +0200

    Created own handler type with serve http func

--- main.go
+++ main.go
@@ -16,22 +16,36 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:test@test.com\">test@test.com</a>.")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		// handle page not found
+// 		// http.NotFound(w, r)
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		// handle page not found
-		// http.NotFound(w, r)
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
+	// http.HandleFunc("/", pathHandler)
 	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
