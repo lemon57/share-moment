dcac0db5be006d622afd94b9b800875b8d65f338
Roman Kislov <gzeek57@gmail.com>
2023-03-06 20:55:45 +0100

    Added templates

--- main.go
+++ main.go
@@ -2,41 +2,43 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my great site!</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email me at "+
-		"<a href=\"mailto:test@test.com\">test@test.com</a>.")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-	<ul>
-	  <li>
-		<b>Is there a free version?</b>
-		Yes! We offer a free trial for 30 days on any paid plans.
-	  </li>
-	  <li>
-		<b>What are your support hours?</b>
-		We have support staff answering emails 24/7, though response
-		times may be a bit slower on weekends.
-	  </li>
-	  <li>
-		<b>How do I contact support?</b>
-		Email us - <a href="mailto:support@sharemoment.com">support@sharemoment.com</a>
-	  </li>
-	</ul>
-	`)
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("processing template: %v", err)
+		http.Error(w, "There was an error processing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
