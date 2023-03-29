45f949e8143af0b0e6d20fe65992549caa204d8a
Roman Kislov <gzeek57@gmail.com>
2023-03-30 00:25:23 +0200

    Added postgres config

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -7,9 +7,32 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
 func main() {
-	db, err := sql.Open("pgx", "postgres://bjj:sharemoment@localhost:5432/sharemoment?sslmode=disable")
-	//db, err := sql.Open("pgx", "host=localhost port=5432 user=bjj password=sharemoment dbname=sharemoment sslmode=disable")
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "bjj",
+		Password: "sharemoment",
+		Database: "sharemoment",
+		SSLMode:  "disable",
+	}
+
+	//db, err := sql.Open("pgx", "postgres://bjj:sharemoment@localhost:5432/sharemoment?sslmode=disable")
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
