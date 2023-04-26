0a33bfe21bf06e3788f80ad76fbd260fa8a0ec77
Roman Kislov <gzeek57@gmail.com>
2023-04-26 22:01:23 +0200

    Prepare session service and use it in users controller

--- models/session.go
+++ models/session.go
@@ -1,7 +1,25 @@
 package models
 
+import "database/sql"
+
 type Session struct {
 	ID        int
 	UserId    int
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO: Create the session token
+	// TODO: Implement SessionService.Create
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO: Implement SessionService.User
+	return nil, nil
 }
