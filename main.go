3306dc921547de46763606aa14836aba8a2101a1
Roman Kislov <gzeek57@gmail.com>
2023-04-28 08:12:10 +0200

    Update session service

--- main.go
+++ main.go
@@ -40,9 +40,13 @@ func main() {
 	userService := models.UserService{
 		DB: db,
 	}
+	sessionService := models.SessionService{
+		DB: db,
+	}
 
 	usersC := controllers.Users{
-		UserService: &userService,
+		UserService:    &userService,
+		SessionService: &sessionService,
 	}
 
 	usersC.Templates.New = views.Must(views.ParseFS(
--- models/session.go
+++ models/session.go
@@ -1,10 +1,18 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/lemon57/share-moment/rand"
+)
 
 type Session struct {
-	ID        int
-	UserId    int
+	ID     int
+	UserID int
+	// Token is only set when creatting a new session. When lokking up a session
+	// this will be left empty, as we only store the hash of a session token
+	// in our database and we cannot reverse it into a raw token.
 	Token     string
 	TokenHash string
 }
@@ -13,10 +21,24 @@ type SessionService struct {
 	DB *sql.DB
 }
 
+// Create will create a new session for the user provided. The session token
+// will be returned as Token field on the Session type, but only hashed
+// session token is stored in the database.
 func (ss *SessionService) Create(userID int) (*Session, error) {
 	// TODO: Create the session token
 	// TODO: Implement SessionService.Create
-	return nil, nil
+	token, err := rand.SessionToken()
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	// TODO: Hash the session token
+	session := Session{
+		UserID: userID,
+		Token:  token,
+		//TODO: set the tokenhash
+	}
+	// TODO: Store he session in our DB
+	return &session, nil
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
